67ef9e938e88d62c11372c996c5406eac7ce943e
KonekoPhysics <89322889+KonekoPhysics@users.noreply.github.com>
2023-11-06 23:23:17 +0900

    Initial commit

---
+++ src/main/java/com/konekophysics/LinkedList.java
@@ -0,0 +1,2 @@
+package com.konekophysics;public class LinkedList {
+}
---
+++ src/main/java/com/konekophysics/Main.java
@@ -0,0 +1,7 @@
+package com.konekophysics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
